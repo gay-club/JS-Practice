023b4f98faf8de151fe818e9be8e86d5f7517d76
Maxim Shtin <maksimshtin@gmail.com>
2024-08-14 15:02:50 +0500

    Add themes: JSON, Promise

--- index.js
+++ index.js
@@ -621,12 +621,13 @@ console.log( isAdmin );
 */
 ///
 
+///
+/*
 let salaries = {
   "John": 100,
   "Pete": 300,
   "Mary": 250
 };
-
 
 function topSalary (obj) {
   let max = 0;
@@ -642,3 +643,102 @@ function topSalary (obj) {
 }
 
 console.log(topSalary(salaries))
+*/
+///
+
+///
+/*
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+let json = JSON.stringify(user)
+
+let obj = JSON.parse(json)
+
+console.log(obj)
+*/
+///
+
+///
+/*
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log( JSON.stringify(meetup, function replacer(key, value) {
+  if (value == meetup && key != "") {
+    return undefined;
+  } else { 
+    return value;
+  }
+
+}));
+
+*/
+///
+
+///
+/*
+function delay(ms) {
+  return new Promise ((resolve) => {
+    // setTimeout(() => {
+    //   resolve()
+    // }, ms);
+    setTimeout(resolve, ms)
+  })
+}
+
+delay(1000).then(() => console.log('выполнилось через 3 секунды'));
+*/
+///
+
+// function go() {
+//   showCircle(150, 150, 100, div => {
+//     div.classList.add('message-ball');
+//     div.append("Привет, мир!");
+//   });
+// }
+
+///
+/*
+function showCircle(cx, cy, radius) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+    return new Promise ((resolve) => {
+      setTimeout(() => {
+        div.style.width = radius * 2 + 'px';
+        div.style.height = radius * 2 + 'px';
+    
+        div.addEventListener('transitionend', function handler() {
+          div.removeEventListener('transitionend', handler);
+          resolve(div);
+        });
+      }, 0);
+  })
+}
+
+function go() {
+  showCircle(150, 150, 100).then(div => {
+    div.classList.add('message-ball');
+    div.append("Hello, world!");
+  });
+}
+*/
+///
