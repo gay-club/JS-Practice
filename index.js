d297015e55c4c5a1d24b0773b10c802b66101931
Maxim Shtin <maksimshtin@gmail.com>
2024-08-16 17:22:59 +0500

    Add theme  async/await functions

--- index.js
+++ index.js
@@ -771,3 +771,102 @@ function doItAFter (sec) {
 doItAFter(3).then(() => console.log('dasdasdas'))
 */
 ///
+
+///
+/*
+async function getState(promise) {
+  let state;
+  let p1 = promise
+    .then(() => state = "fulfilled")
+    .catch(() => state = "rejected");
+
+  let p2 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 100);
+  }).then(() => (state = "pending"));
+  return Promise.race([p1, p2]);
+}
+
+const promise = new Promise((resolve, rejected) => {
+    resolve()
+});
+
+console.log(getState(promise));
+*/
+///
+
+///
+/*
+async function loadJson (url) {
+  let response = await fetch(url)
+  if (response.status == 200) {
+    return response.json()
+  } else {
+    throw new Error(response.status);
+  }
+}
+
+loadJson('no-such-user.json') // (3)
+  .catch(alert); // Error: 404
+*/
+///
+
+///
+/*
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+async function loadJson (url) {
+  let response = await fetch(url)
+  if (response.status == 200) {
+    return response.json()
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+async function demoGithubUser () {
+  let user
+  while (true) {
+    try {
+      let name = prompt("Введите логин?", "iliakan")
+      user = await loadJson(`https://api.github.com/users/${name}`)
+      break;
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        console.log("Такого пользователя не существует, пожалуйста, повторите ввод.");
+      } else {
+        throw err;
+      }
+    }
+  }
+  console.log(`Полное имя: ${user.name}.`);
+  return user
+}
+
+demoGithubUser();
+*/
+///
+
+///
+/*
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return 10;
+}
+
+function f() {
+  wait().then((res) => {
+    console.log(res)
+  })
+}
+
+console.log(f())
+*/
+///
